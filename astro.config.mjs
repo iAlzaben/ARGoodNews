f359d1324ac33b81b74a6db736589372d10b9d24
iAlzaben <ibraheemb@gmail.com>
2026-05-24 22:38:34 -0700

    Configure Astro theme for GitHub Pages automated deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  // Enable static site generation (Astro's default high-performance output)
   output: 'static',
+  site: 'https://iAlzaben.github.io',
+  base: '/ARGoodNews',
 });
